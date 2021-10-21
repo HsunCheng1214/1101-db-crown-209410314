6b94de11eea84a0a34580cd413d45b5eddb41422
yourname <209410314@gms.tku.edu.tw>
2021-10-21 21:56:45 +0800

    cors change

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   cors({
-    origin: ['https://crown1101.herokuapp.com']
+    origin: '#'
   })
 );
 app.use('/', indexRouter);
