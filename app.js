8bbb482fd9aa2028f223d16277a5e65f8943e152
yourname <209410314@gms.tku.edu.tw>
2021-10-21 20:44:01 +0800

    week5 first

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,12 +22,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(
+  cors({
+    origin: ['https://crown1101.herokuapp.com']
+  })
+);
 app.use('/', indexRouter);
 app.use('/crown_14', crown_14_Router);
 app.use('/crown2_14', crown2_14_Router);
 app.use('/users', usersRouter);
-app.use('/api_14',api_14_Router)
+app.use('/api_14', cors(), api_14_Router);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
